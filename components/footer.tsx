2c31ac9d4a68678bda437ccf2d65cde7cd107be3
Lilit <baregamyanlilit36@gmail.com>
2026-04-10 01:06:51 +0400

    .

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,9 @@ export function Footer() {
   return (
     <footer className="FontArmDecorativeU bottom-0 py-10 text-center text-[#4b4b4b]  bg-[#F4F4F4] ">
       
-      <Link href="https://www.instagram.com/webinvite.am/">
+      <Link href="https://www.instagram.com/siteup.am/">
         <p className="mt-2 text-base">
-          Պատրաստվել է <span className="underline">WebInvite</span> -ի  կողմից 
+          Պատրաստվել է <span className="underline">siteup.am</span> -ի  կողմից 
         </p>
       </Link>
     </footer>
